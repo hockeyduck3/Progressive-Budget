43b71403985ec7ff0cd9d9dbb849c428a1ac10ea
LJ Spencer <hockeyduck3@gmail.com>
2020-07-04 19:01:41 -0600

    Added some of the code needed for the offline.js file

--- public/offline.js
+++ public/offline.js
@@ -1,1 +1,21 @@
-const db;
+let db;
+
+const index = indexedDB.open('budget', 1);
+
+index.onupgradeneeded = event => {
+    const db = event.target.result;
+    db.createObjectStore('pending', { autoIncrement: true });
+}
+
+index.onsuccess = event => {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        // If the app is online run this function to see if there is anything saved in the database.
+        runCheck();
+    }
+}
+
+index.onerror = event => {
+    console.log(`There was an error: ${event.target.errorCode}`)
+}
